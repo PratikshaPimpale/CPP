5492eb76f1c030cbef48a1b14837cfb0dca035a8
Pratiksha Pimpale <155970383+PratikshaPimpale@users.noreply.github.com>
2024-01-08 19:35:51 +0530

    Add files via upload

---
+++ para_constructor.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+class emp
+{
+	public:
+		int eno;
+		char ename[20];
+		float sal;
+		
+		emp(int eno,char ename[20], float sal)
+		{
+			this->eno=eno;
+			strcpy(this->ename,ename);
+			this->sal=sal;
+		}
+		
+		void disp()
+		{
+			cout<<"Emp no="<<eno<<endl;
+			cout<<"Emp name="<<ename<<endl;
+			cout<<"Emp salary="<<sal<<endl;
+		}
+};
+		
+int main()
+{
+	int eno;
+	char ename[20];
+	float sal;
+	
+	cout<<"Enter emp no name salary: ";
+	cin>>eno>>ename>>sal;
+	
+	emp ob(eno,ename,sal);
+	ob.disp();
+}
